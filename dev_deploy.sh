742975c5605eb56a7cfefa6eb46af0ec495d50cc
Darian Hickman <darian.hickman@villagethegame.com>
2017-05-29 14:59:00 -0700

    Got simpler auth working

--- dev_deploy.sh
+++ dev_deploy.sh
@@ -7,13 +7,12 @@ export CLOUDSDK_CORE_DISABLE_PROMPTS=1
 export CLOUDSDK_CORE_PROJECT=villagegamedev
 export CONFIG_DOCID=1r7LKoEI-1kFhSnH75XwEP1zdkfQzdN6LObM4MJLtiaw
 
-
 if [ ! -f /usr/local/bin/node ]
 then
 	brew install node
 fi
 
-if [ ! -f ${HOME}/google-cloud-sdk/bin ]
+if [ ! -e ${HOME}/google-cloud-sdk/bin ]
 then
 	SDK_VERSION=127.0.0
 	SDK_FILENAME=google-cloud-sdk-${SDK_VERSION}-linux-x86_64.tar.gz
@@ -22,7 +21,7 @@ then
 	export PATH=${PATH}:${HOME}/google-cloud-sdk/bin
 fi
         # Install ige
-if [ ! -f ige/server ]  
+if [ ! -e ige/server ]  
 then
 	curl -OL https://github.com/Irrelon/ige/archive/v1.5.5@2014-09-29-merge.tar.gz
 	tar -zxvf v1.5.5@2014-09-29-merge.tar.gz
@@ -34,7 +33,7 @@ cd ige/server
 npm install
 cd ../..
 
-if [ ! -f ${HOME}/google_appengine/appcfg.py ]
+if [ ! -e ${HOME}/google_appengine/appcfg.py ]
 then    
 	# Install Google App Engine SDK
 
@@ -46,8 +45,12 @@ fi
 
         # Install app & dev dependencies, deploy
 pip --quiet install -r requirements.txt -t libs/
-echo "key = '${GOOGLE_API_KEY}'" > api_key.py
-#echo ${GOOGLE_CLIENT_SECRET} > client-secret.json
+
+if [ ! -e client-secret.json]
+then
+	cp villagegamedev-7ef4100fcad6.json ./client-secret.json
+fi
+
 python create_config_yaml.py ${CONFIG_DOCID}
 cd ige
 node server/ige.js -deploy ../client
